f8dc0fb0e9c3fe28c413003e896e1c7f3ade4107
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2023-05-27 09:13:47 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from "./components/Navbar.js";
+import NaavBar from "./components/Navbar.js";
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <NaavBar />
     </div>
   );
 }
